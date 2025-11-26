a310722f3d8691561b10715a93bede8f987ccf0c
Jeroen Coekaerts <jeroen@dnxnetwork.lu>
2025-11-26 22:24:24 +0100

    fix

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="cgwatch",
     version="0.1.0",
-    packages=find_packages(),
-    # Scripts are handled manually in debian/rules to rename them
+    packages=["cgwatch"],
+    include_package_data=True
 )
